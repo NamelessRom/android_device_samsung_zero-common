1e4707de79bbb6829395dcc3f26f5509114fa0be
Javi Ferrer <javi.f.o@gmail.com>
2016-04-28 18:40:15 +0200

    namelessify
    Change-Id: Iba56f6a2012c2a013aec973c4e6cb8cd047948b6

--- zero-common.mk
+++ zero-common.mk
@@ -197,9 +197,5 @@ PRODUCT_COPY_FILES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
-# call Samsung LSI board support package
-$(call inherit-product, hardware/samsung_slsi-cm/exynos5/exynos5.mk)
-$(call inherit-product, hardware/samsung_slsi-cm/exynos7420/exynos7420.mk)
-
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/samsung/zero-common/zero-common-vendor.mk)
