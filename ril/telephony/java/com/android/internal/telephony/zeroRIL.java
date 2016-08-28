764817493dcd09bb861b6e91d4c5b5f145eb60c3
Astrubale <mahiri0575@gmail.com>
2016-08-28 19:56:31 +0200

    Update zeroRIL.java

--- ril/telephony/java/com/android/internal/telephony/zeroRIL.java
+++ ril/telephony/java/com/android/internal/telephony/zeroRIL.java
@@ -40,6 +40,8 @@ import com.android.internal.telephony.uicc.IccCardStatus;
 public class zeroRIL extends RIL implements CommandsInterface {
 
     private boolean DBG = false;
+    private boolean isGsm = false;
+
 
     public zeroRIL(Context context, int preferredNetworkType,
             int cdmaSubscription, Integer instanceId) {
@@ -96,6 +98,12 @@ public class zeroRIL extends RIL implements CommandsInterface {
         return cardStatus;
     }
 
+   @Override
+    public void setPhoneType(int phoneType){
+        super.setPhoneType(phoneType);
+        isGsm = (phoneType != RILConstants.CDMA_PHONE);
+    }
+
     protected Object
     responseCallList(Parcel p) {
         int num;
@@ -120,17 +128,28 @@ public class zeroRIL extends RIL implements CommandsInterface {
             dc.isMpty = (0 != p.readInt());
             dc.isMT = (0 != p.readInt());
             dc.als = p.readInt();
-            voiceSettings = p.readInt();
-            dc.isVoice = (0 == voiceSettings) ? false : true;
-            p.readInt(); // samsung call detail
-            p.readInt(); // samsung call detail
-            p.readString(); // samsung call detail
+            dc.isVoice = (0 != p.readInt());
+
+            if (!isGsm) {
+                int call_type = p.readInt(); // samsung call detail
+                int call_domain = p.readInt(); // samsung call detail
+                String csv = p.readString(); // samsung call detail
+            }
+
             dc.isVoicePrivacy = (0 != p.readInt());
             dc.number = p.readString();
-            int np = p.readInt();
-            dc.numberPresentation = DriverCall.presentationFromCLIP(np);
+            if (RILJ_LOGV) {
+                riljLog("responseCallList dc.number=" + dc.number);
+            }
+
+            dc.numberPresentation = DriverCall.presentationFromCLIP(p.readInt());
             dc.name = p.readString();
-            dc.namePresentation = p.readInt();
+            if (RILJ_LOGV) {
+                riljLog("responseCallList dc.name=" + dc.name);
+            }
+            // according to ril.h, namePresentation should be handled as numberPresentation;
+            dc.namePresentation = DriverCall.presentationFromCLIP(p.readInt());
+
             int uusInfoPresent = p.readInt();
             if (uusInfoPresent == 1) {
                 dc.uusInfo = new UUSInfo();
